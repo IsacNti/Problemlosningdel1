bcf9757f5f4b758b65b4e8985ba0f894134db9f9
IsacNti <77719323+IsacNti@users.noreply.github.com>
2022-04-04 11:20:35 +0200

    Initial commit

---
+++ src/finalize.java
@@ -0,0 +1,18 @@
+class om {
+    public static String reverseString(String str){
+        StringBuilder sb=new StringBuilder(str);
+        sb.reverse();
+        return sb.toString();
+    }
+
+    public static String capitalizeWord(String str){
+        String words[]=str.split("\\s");
+        String capitalizeWord="";
+        for(String w:words){
+            String first=w.substring(0,1);
+            String afterfirst=w.substring(1);
+            capitalizeWord+=first.toUpperCase()+afterfirst+" ";
+        }
+        return capitalizeWord.trim();
+    }
+}
---
+++ src/test.java
@@ -0,0 +1,17 @@
+import static com.sun.xml.internal.ws.util.StringUtils.capitalize;
+
+public class test {
+
+
+    public static void main(String[] args) {
+        System.out.println(om.reverseString("vad cool"));
+        System.out.println(om.reverseString("livet svårt"));
+        System.out.println(om.capitalizeWord("mitt förnamn och efternamn"));
+        System.out.println(om.capitalizeWord("I am sonoo jaiswal"));
+    }
+}
+
+
+
+
+
